1166de30751d2785bf85c4b9dc02823478eb6482
Will <wrjones104>
2022-09-21 06:28:50 -0400

    Fixed issue where users could submit times more than once Added duplicate command cleanup on connect

--- main.py
+++ main.py
@@ -19,9 +19,10 @@ class aclient(discord.Client):
 
     async def on_ready(self):
         await self.wait_until_ready()
-        # tree.clear_commands(guild=None)
         if not self.synced:
-            await tree.sync()
+            # await tree.sync(guild=discord.Object(id=ids.ff6wc_guild))
+            await tree.sync(guild=discord.Object(id=ids.ff6wc_guild))
+            await tree.sync(guild=None)
             self.synced = True
         print(f"We have logged in as {self.user}.")
 
@@ -100,7 +101,7 @@ async def refresh_sotw(interaction: Interaction):
     else:
         await interaction.response.send_message(f"Only Racebot Admins can use this command.", ephemeral=True)
 
-
-tree.add_command(sotw_group)
+tree.add_command(sotw_group, guild=discord.Object(ids.ff6wc_guild))
+tree.clear_commands(guild=None)
 
 client.run(os.getenv('discord_token'))

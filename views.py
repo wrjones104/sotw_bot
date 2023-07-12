9fcbff7b571b7f513c8a7c6e356adee886442936
Will Jones <wrjones@gmail.com>
2023-07-12 13:51:35 -0400

    Add automated rolling * Added ability for bot to automatically roll the SotW at the same time each week ** The bot will randomly choose a vetted flagset from a Google Sheet to roll for the week *** The bot will move successfully rolled flagsets from the **Submissions** tab to the **Rolled Seeds** tab ** The bot will ignore any submission from the person whose SotW was chosen in the previous week ** If there are no vetted flagsets, the bot will choose between rolling a chaos flagset (50%) or a flagset from the reserves (50%) ** The bot will verify that the flags work - if they don't, it will "quarantine" that flagset and move on to another random selection * Added the ability for users to submit their ideas through the bot (/sotw submit) * Added the ability for Racebot Admins to submit reserve flagsets (/sotw reserve) * Added the ability for Racebot Admins to turn the "Auto Roll" functionality on or off (/sotw auto <True/False>)

--- views.py
+++ views.py
@@ -3,7 +3,8 @@ import json
 import discord
 from discord.ui import View
 
-from functions.command_functions import generate_seed, get_flags
+import functions.command_functions
+from functions.command_functions import generate_seed, get_flags, new_submission
 
 
 class SotwPingView(View):
@@ -19,10 +20,15 @@ class SotwPingView(View):
             with open("sotw_db.json") as x:
                 sotw_db = json.load(x)
                 flags = await get_flags(sotw_db[str(len(sotw_db))]['seed_id'])
-            seedlink = await generate_seed(flags, False, interaction)
+            seedlink = await generate_seed(flags, False)
             await interaction.followup.send(f'Here\'s your practice seed - good luck!\n{seedlink["url"]}',
                                             ephemeral=True)
         except (discord.errors.HTTPException, discord.errors.NotFound):
             await interaction.response.defer()
             await interaction.followup.send(f"I'm a little overloaded - give me a sec and try again",
                                             ephemeral=True)
+
+    @discord.ui.button(label="Submit Idea", style=discord.ButtonStyle.green, emoji="💡",
+                       custom_id="idea_button")
+    async def submit_idea(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await functions.command_functions.new_submission(interaction)
